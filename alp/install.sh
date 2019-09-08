2c1d783918324ba7725d7e3c87edf1c58529fb90
HardWolf <hardwolf@HardWolfnoMacBook-Pro.local>
2019-09-08 13:00:19 +0900

    rapid commit

--- alp/install.sh
+++ alp/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get install wget
+apt-get install wget unzip
 wget https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
+mv alp_linux_amd64.zip $HOME/
 unzip alp_linux_amd64.zip
 mv alp /usr/local/bin/
